e1cdf6db901bc880d52846e26c32de5e0145f904
SonixDev131 <son.nl215131@gmail.com>
2025-05-12 22:45:11 +0700

    chore: Clean up project structure and remove unused files
    - Deleted agent_config.json, build_installer.py, installer.py, README.md, and requirements.txt to streamline the codebase.
    - This cleanup enhances maintainability by eliminating obsolete components.

---
+++ boxstarter.ps1
@@ -0,0 +1,19 @@
+## This command disables User Account Control to run the script without user interaction, it is enabled at the end of the script.
+## To avoid security concerns you can comment it if you prefer, otherwhise please check the software you install is safe and use this command at your own risk.
+Disable-UAC
+$Boxstarter.AutoLogin=$false
+# Install git and clone repository containing scripts and config files
+# TODO: see how to improve install that by using chezmoi (choco install -y chezmoi)
+choco install -y git --params "/GitOnlyOnPath /NoShellIntegration /WindowsTerminal"
+choco install -y nssm
+choco install -y python 
+RefreshEnv
+git clone https://github.com/SonixDev131/lab-agent.git "$env:USERPROFILE\lab-agent"
+
+nssm install "agent" python "$env:USERPROFILE\lab-agent\main.py"
+nssm start agent
+
+#--- reenabling critial items ---
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
