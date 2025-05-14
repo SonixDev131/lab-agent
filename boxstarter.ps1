a2138c7aeca0600c3c8e9a92817ab7c56ae27023
SonixDev131 <son.nl215131@gmail.com>
2025-05-14 09:16:27 +0700

    chore: Update Boxstarter script formatting and install commands
    - Adjusted spacing for better readability in boxstarter.ps1.
    - Ensured consistent formatting for AutoLogin assignment.
    - Updated Python installation command for clarity.

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -1,12 +1,12 @@
 ## This command disables User Account Control to run the script without user interaction, it is enabled at the end of the script.
 ## To avoid security concerns you can comment it if you prefer, otherwhise please check the software you install is safe and use this command at your own risk.
 Disable-UAC
-$Boxstarter.AutoLogin=$false
+$Boxstarter.AutoLogin = $false
 # Install git and clone repository containing scripts and config files
 # TODO: see how to improve install that by using chezmoi (choco install -y chezmoi)
 choco install -y git --params "/GitOnlyOnPath /NoShellIntegration /WindowsTerminal"
 choco install -y nssm
-choco install -y python 
+choco install -y python
 RefreshEnv
 git clone https://github.com/SonixDev131/lab-agent.git "$env:USERPROFILE\lab-agent"
 
